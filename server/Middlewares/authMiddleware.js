79ddff5bddd9c442229956bafdc82e37c876e7db
HYUN <8135zxcvbn@gmail.com>
2024-12-05 17:04:52 +0900

    [Fix] authMiddleware.js - 프로필 이미지 로딩으로 리소스 비교 주석화

--- server/Middlewares/authMiddleware.js
+++ server/Middlewares/authMiddleware.js
@@ -7,11 +7,11 @@ const authMiddleware = (req, res, next) => {
 
 
     // 요청된 리소스의 사용자 ID와 현재 로그인한 사용자 ID 비교
-    const requestedUserId = req.params.user_id || req.body.user_id;
+    // const requestedUserId = req.params.user_id || req.body.user_id;
     
-    if (requestedUserId && req.session.user.user_id !== requestedUserId) {
-        return res.status(403).json({message: 'unauthorized_access'});
-    }
+    // if (requestedUserId && req.session.user.user_id !== requestedUserId) {
+    //     return res.status(403).json({message: 'unauthorized_access'});
+    // }
     // console.log('인증 성공');
 
     //req 객체에 사용자 정보 추가
