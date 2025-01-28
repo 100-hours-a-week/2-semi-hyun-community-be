0b606d4f476b812adb174b3d43c0d6c17ccd719a
HYUN <8135zxcvbn@gmail.com>
2025-01-28 23:20:19 +0900

    [Update] Configuration for Deployment
    - Updated .env.example and express-config.js to use new server IP
    - Modified CORS and Content Security Policy settings to match new frontend server address

--- server/config/express-config.js
+++ server/config/express-config.js
@@ -1,5 +1,5 @@
 export const PORT = 3000;
 export const CORS_OPTIONS = {
-    origin : 'http://localhost:8081', //프론트 서버 주소
+    origin : 'http://172.31.42.82:8081', //프론트 서버 주소
     credentials : true
   }
